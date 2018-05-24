21855073695d375a8c801f31923269012cf3cb95
jerabaul29 <jean.rblt@gmail.com>
2018-05-24 11:06:11 +0200

    v2 ready

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
+    version='2.0',
 
     description='Call all functions in a file',
     # long_description=long_description,
